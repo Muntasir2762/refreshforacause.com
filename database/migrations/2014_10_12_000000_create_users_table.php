25659128ec0c9fa6aa5c3a2ca4cdd99db580e15c
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-11-15 02:39:58 +0600

    user and site_settings migration done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('unique_ref')->unique();
+            $table->string('role')->default('user')->comment('companyadmin', 'orgadmin', 'orgmember', 'user');
+            $table->unsignedBigInteger('org_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('full_name_slug');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('status')->default('active')->comment('active', 'inactive', 'suspended');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
