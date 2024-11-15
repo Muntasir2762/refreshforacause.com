7148950005840ab012da0bd8ab8d596ba1d13071
salehibneomar <salehibneomar@gmail.com>
2024-11-16 01:28:16 +0600

    added toaster

---
+++ public/admin/assets/js/toastr-setup.js
@@ -0,0 +1,18 @@
+toastr.options = {
+    positionClass: "toast-top-right",
+    timeOut: 5e3,
+    closeButton: !0,
+    debug: !1,
+    newestOnTop: !0,
+    progressBar: !0,
+    preventDuplicates: !0,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+    tapToDismiss: !1
+  }
