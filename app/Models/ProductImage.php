d618820465e632e1045db0f701eca56e993b357a
Saleh Ibne Omar <35599978+salehibneomar@users.noreply.github.com>
2024-11-28 00:32:55 +0600

    added product image model migration relationship (#14)

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductImage extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    const IMAGE_DIR = 'images/product/others/';
+
+    protected $fillable = [
+        'name',
+        'product_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+}
